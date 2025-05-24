9eaea52252816deb762e40083bdc93d405135272
Paul Hlusko <paul.hlusko@gmail.com>
2025-05-24 10:23:34 -0400

    two fresh apps

---
+++ FormsAndListsiOS/FormsAndListsiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FormsAndListsiOS
+//
+//  Created by Paul Hlusko on 5/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
